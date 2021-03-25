fde518c0270aa6125c24e5696bf5adcfa2a7260f
thanwin84 <60491531+thanwin84@users.noreply.github.com>
2021-03-26 02:29:59 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,53 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-int *add_one(vector<int> arr, int size){
-    // creating dynamic array of size "size" with new keyword
-    int  *myArray = new int[size];
-    for (int i=0; i < size; i++){
-        myArray[i] = arr[i] + 1;
-    }
-    return myArray;
 
-}
+class Pair{
+public:
+    int value;
+    int li;
+    int di;
+public:
+    Pair(int value, int li, int di){
+        this->value = value;
+        this->li = li;
+        this->di = di;
+    }
+
+};
+auto comp =[](Pair &a, Pair &b){
+    return a.value > b.value;
+};
+class Solution{
+public:
+    vector<int> mergeKArrays(vector<vector<int>> arr, int k)
+    {
+        //code here
+        priority_queue<Pair, vector<Pair>,decltype(comp)> storage(comp);
+        vector<int> result;
+        for(int i = 0; i < arr.size(); i++){
+            storage.push(Pair(arr[i][0], i, 0));
+        }
+
+        while(!storage.empty()){
+            Pair pair = storage.top();
+            result.push_back(pair.value);
+            storage.pop();
+            pair.di += 1;
+            if (pair.di < arr[pair.li].size()){
+                storage.push(Pair(arr[pair.li][pair.di], pair.li, pair.di));
+            }
+        }
+        return result;
+    }
+};
 int main() {
-   auto a = 10000009000000009;
-   vector<int> arr = {1, 2, 3, 4};
-   int *pointer = add_one(arr, 4);
-   for (int i = 0; i < 4; i++){
-       cout << pointer[i] << " ";
-
-   }
-   // deallocating memory
-   delete pointer;
-   pointer = nullptr;
-    for (int i = 0; i < 4; i++){
-        cout << pointer[i] << " ";
-
+    vector<vector<int>> data = {{1, 2, 3},
+                                {4, 5, 6},
+                                {7, 8, 9}};
+   Solution obj;
+   vector<int> result = obj.mergeKArrays(data, 3);
+    for (auto i : result){
+        cout << i << " ";
     }
-    return 0;
+
 }
